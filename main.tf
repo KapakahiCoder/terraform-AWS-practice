84019d7ea8e1afb869feaceeff529f0016dc11f5
Garrett Chun <rubbishman@gmail.com>
2023-09-26 04:01:00 +0000

    #19 change structure of root module and add tagging to bucket

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "Kapakahi"
-
-    workspaces {
-      name = "terra-house"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
 
 resource "random_string" "bucket_name" {
   lower = true
@@ -37,8 +9,7 @@ resource "random_string" "bucket_name" {
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
