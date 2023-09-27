2744b3fe730cfeb2b9875b901d2a10f61ba522eb
Garrett Chun <rubbishman@gmail.com>
2023-09-27 03:57:10 +0000

    #25 Refacto s3 bucket into new terrahouse_aws module

--- variable.tf
+++ variable.tf
@@ -1,18 +1,7 @@
 variable "user_uuid" {
-  description = "User UUID"
   type        = string
-  validation {
-    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid))
-    error_message = "User UUID must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)"
-  }
 }
 
 variable "bucket_name" {
-  description = "The name of the AWS S3 bucket"
   type        = string
-
-  validation {
-    condition     = can(regex("^[a-z0-9][a-z0-9.-]{1,61}[a-z0-9]$", var.bucket_name))
-    error_message = "Bucket name must be between 3 and 63 characters, contain only lowercase letters, numbers, hyphens, and periods, and start and end with a letter or number."
-  }
 }
